cc8b93a6591e8ad5c5bbd90e56931085730f7a8b
Kuba <kubaz9991@gmail.com>
2019-07-15 23:30:29 +0200

    create global style, styled wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,28 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled, { createGlobalStyle } from "styled-components"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import SEO from "../components/seo"
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+`
+
+const GlobalStyle = createGlobalStyle`
+  body {
+   background-color: red;
+   margin: 0;
+  }
+`
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+  <StyledWrapper>
+    <GlobalStyle />
+    {/* <SEO title="Home" /> */}
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </StyledWrapper>
 )
 
 export default IndexPage
