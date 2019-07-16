3045cd09dd05aea9cc4879db0d5808a42028758f
Kuba <kubaz9991@gmail.com>
2019-07-16 23:16:02 +0200

    added pages

---
+++ src/pages/contact.js
@@ -0,0 +1,32 @@
+import React from "react"
+import styled from "styled-components"
+import Header from "../components/header"
+
+// import SEO from "../components/seo"
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #ff7659;
+`
+
+const StyledMain = styled.main`
+  width: 100%;
+  height: 90vh;
+  background-color: #0a7caf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Contact = () => (
+  <StyledWrapper>
+    <Header />
+    <StyledMain>
+      {/* <SEO title="Home" /> */}
+
+      <h1>hi from contact</h1>
+    </StyledMain>
+  </StyledWrapper>
+)
+
+export default Contact
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,22 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
-import { createGlobalStyle } from "styled-components"
+import styled from "styled-components"
+import Header from "../components/header"
 
-const GlobalStyle = createGlobalStyle`
-  body {
-   background-color: red;
-   margin: 0;
-   font-family: "Nunito"
-  }
+// import SEO from "../components/seo"
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #ff7659;
+`
+
+const StyledMain = styled.main`
+  width: 100%;
+  height: 90vh;
+  background-color: #0a7caf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Projects = () => (
-  <>
-    <GlobalStyle />
+  <StyledWrapper>
+    <Header />
+    <StyledMain>
+      {/* <SEO title="Home" /> */}
 
-    <h1>Hi from the second page</h1>
-    <Link to="/">Go back to the homepage</Link>
-  </>
+      <h1>hi from projects</h1>
+    </StyledMain>
+  </StyledWrapper>
 )
 
 export default Projects
