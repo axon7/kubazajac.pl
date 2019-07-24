c86ff8d74ff42bf26a60c79f7d0afa495aac0fcc
Kuba <kubaz9991@gmail.com>
2019-07-24 19:24:38 +0200

    footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,25 +4,34 @@ import styled from "styled-components"
 import { FaLinkedin, FaGithub } from "react-icons/fa"
 
 const StyledFooter = styled.footer`
-  background-color: black;
+  background-color: #663399;
   height: 15vh;
   width: 100%;
   color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
-const StyledP = styled.p`
-  margin: 0 auto;
-  text-align: center;
+const StyledIconsDiv = styled.div`
   font-size: 60px;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  max-width: 600px;
 `
 // import { Helmet } from "react-helmet"
 
 const Footer = () => (
   <StyledFooter>
-    <StyledP>
-      <FaLinkedin />
-      <FaGithub />
-    </StyledP>
+    <StyledIconsDiv>
+      <a href="https://github.com/axon7" style={{ color: "white" }}>
+        <FaGithub style={{ marginRight: "10px" }} />
+      </a>
+      <a href="https://linkedin.com" style={{ color: "white" }}>
+        <FaLinkedin style={{ marginLeft: "10px" }} />
+      </a>
+    </StyledIconsDiv>
   </StyledFooter>
 )
 
